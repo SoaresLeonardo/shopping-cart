42d79583543345b237d413197f1b3d045ff50dda
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-02-10 23:15:19 -0300

    Create Data.js

---
+++ app/backend/Data.js
@@ -0,0 +1,62 @@
+const data = {
+  productItems: [
+    {
+      id: "1",
+      name: "Sapato exemplo",
+      description: "Descrição exemplo",
+      price: "190.20",
+      image: "../src/assets/img/tenis.png",
+    },
+    {
+      id: "2",
+      name: "Sapato exemplo",
+      description: "Descrição exemplo",
+      price: "190.20",
+      image: "../src/assets/img/tenis.png",
+    },
+    {
+      id: "3",
+      name: "Sapato exemplo",
+      description: "Descrição exemplo",
+      price: "190.20",
+      image: "../src/assets/img/tenis.png",
+    },
+    {
+      id: "4",
+      name: "Sapato exemplo",
+      description: "Descrição exemplo",
+      price: "190.20",
+      image: "../src/assets/img/tenis.png",
+    },
+    {
+      id: "5",
+      name: "Sapato exemplo",
+      description: "Descrição exemplo",
+      price: "190.20",
+      image: "../src/assets/img/tenis.png",
+    },
+    {
+      id: "6",
+      name: "Sapato exemplo",
+      description: "Descrição exemplo",
+      price: "190.20",
+      image: "../src/assets/img/tenis.png",
+    },
+    {
+      id: "7",
+      name: "Sapato exemplo",
+      description: "Descrição exemplo",
+      price: "190.20",
+      image: "../src/assets/img/tenis.png",
+    },
+    {
+      id: "8",
+      name: "Sapato exemplo",
+      description: "Descrição exemplo",
+      price: "190.20",
+      image: "../src/assets/img/tenis.png",
+    },
+  ],
+};
+
+export default data;
