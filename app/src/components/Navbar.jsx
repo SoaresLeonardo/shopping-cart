f898292e7dc9cac3b43f67b8533f649dd0d16bb2
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-02-12 23:12:21 -0300

    settings products and navbar

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,19 +1,18 @@
-import { AiOutlineShoppingCart, AiOutlineHome } from "react-icons/ai";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 import { Link } from "react-router-dom";
-import {BiRocket} from 'react-icons/bi'
+import { BiRocket } from "react-icons/bi";
 import "./Navbar.css";
 export default function Navbar() {
   return (
     <>
       <nav>
-        <div className="nav-name">Shopping cart <BiRocket/></div>
+        <div className="nav-name">
+          <Link to="/" className="home-link">
+            Shopping cart <BiRocket />
+          </Link>
+        </div>
         <div className="nav-links">
           <ul>
-            <li>
-              <Link to="/">
-                <AiOutlineHome className="icon" />
-              </Link>
-            </li>
             <li>
               <Link to="/cart">
                 <AiOutlineShoppingCart className="icon" />
