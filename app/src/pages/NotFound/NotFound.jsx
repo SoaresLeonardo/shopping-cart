71a59ab0af9d1c519371d9f7ae0f81a9eeb59514
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-02-17 12:00:13 -0300

    settings page notFound

--- app/src/pages/NotFound/NotFound.jsx
+++ app/src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,27 @@
 import "./NotFoundStyles.css";
+import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
 
 export default function NotFound() {
-  return <div className="">
-    <h1>Error</h1>
-  </div>;
+  return (
+    <div>
+      <div className="notFound">
+        <h1>Ops... ocorreu erro</h1>
+        <span>Voltar para <a href="/">home</a></span>
+        <div className="notFound-image">
+          <img
+            src="../../../public/img/notFoundImage.png"
+            alt="Imagem de erro"
+          />
+        </div>
+        <div className="sociais">
+          <a href="">
+            <AiFillGithub />
+          </a>
+          <a href="">
+            <AiFillInstagram />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
