83a50b4a1d29ca2ef06c004c626171840f3c37e3
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-02-18 00:02:30 -0300

    Update NotFound.jsx

--- app/src/pages/NotFound/NotFound.jsx
+++ app/src/pages/NotFound/NotFound.jsx
@@ -2,10 +2,12 @@ import "./NotFoundStyles.css";
 import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
 
 export default function NotFound() {
+  document.title = "Not Found";
+  
   return (
     <div>
       <div className="notFound">
-        <h1>Ops... ocorreu erro</h1>
+        <h1>Ops... ocorreu erro 404</h1>
         <span>
           Voltar para <a href="/">home</a>
         </span>
