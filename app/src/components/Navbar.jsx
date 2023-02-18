3d02b3ba47ff6c47c47c1be29516b9761fdd4c83
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-02-18 15:55:07 -0300

    Update Navbar.jsx

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -3,25 +3,22 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 export default function Navbar({ cart }) {
   return (
-    <>
-      <nav>
-        <div className="nav-name">
-          <Link to="/" className="home-link">
-            Shopping cart
-          </Link>
-        </div>
-        <div className="nav-links">
-          <ul>
-            <li>
-              <Link to="/cart">
-                <AiOutlineShoppingCart className="icon" />
-              </Link>
-
-              <span className="cartItems-counter">{cart.length}</span>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </>
+    <nav>
+      <div className="nav-name">
+        <Link to="/" className="home-link">
+          Shopping cart
+        </Link>
+      </div>
+      <div className="nav-links">
+        <ul>
+          <li>
+            <Link to="/cart">
+              <AiOutlineShoppingCart className="icon" />
+            </Link>
+            <span className="cartItems-counter">{cart.length}</span>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
