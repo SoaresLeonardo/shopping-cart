6a709269648dec1b52e9b647340bdd735e61d046
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-02-18 13:47:41 -0300

    new style

--- app/src/pages/Cart/Cart.jsx
+++ app/src/pages/Cart/Cart.jsx
@@ -1,60 +1,76 @@
 import Navbar from "../../components/Navbar";
-import "./styles.css";
-import { IoMdRemove, IoMdAdd } from "react-icons/io";
 import { FaCartPlus } from "react-icons/fa";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+import "./styles.css";
 
-export default function Cart({ cart }) {
-  const priceTotal = cart.reduce(
+export default function Cart({ cart, addToCart }) {
+  const price = cart.reduce(
     (price, item) => price + item.quantity * item.price,
     0
   );
+  const frete = 15.9;
+  const priceTotal = price + frete;
 
   return (
-    <>
-      <div className="container">
-        <Navbar cart={cart} />
-        <div className="cartItems-title">
-          <h1>Meu carrinho</h1>
-        </div>
-        {cart.length > 0 ? (
+    <div className="container">
+      <Navbar cart={cart} />
+      <div className="cartItems-container">
+        {cart.map((item) => (
           <div className="cartItems">
-            {cart.map((item) => (
-              <div key={item.id} className="cartItem-container">
-                <div className="cartItem-image">
-                  <img
-                    src={item.image}
-                    alt="Imagem do produto dentro do carrinho"
-                  />
-                </div>
-                <div className="cartItem-description">
-                  <span>{item.description}</span>
-                </div>
-                <div className="cartItem-functions">
-                  <button>
-                    <IoMdRemove />
-                  </button>
-                  <span className="cartItem-quanty">{item.quantity}</span>
-                  <button>
-                    <IoMdAdd />
-                  </button>
-                </div>
-                <div className="cartItem-price">{`R$ ${item.price}`}</div>
-              </div>
-            ))}
-            <div className="finalize">
-              <button className="payment">Comprar</button>
-              <span className="total">
-                Total: {`R$: ${priceTotal.toFixed(2)}`}
-              </span>
+            <div className="cartItem-image">
+              <img src={item.image} alt="Imagem do Produto" />
+            </div>
+            <div className="cartItem-description">
+              <h4>Produto</h4>
+              <span>{item.description}</span>
+            </div>
+            <div className="cartItem-functions">
+              <button>
+                <AiOutlineLeft />
+              </button>
+              <span>{item.quantity}</span>
+              <button onClick={() => addToCart(item)}>
+                <AiOutlineRight />
+              </button>
+            </div>
+            <div className="cartItem-price">
+              <span>{`R$ ${item.price}`}</span>
             </div>
           </div>
-        ) : (
-          <div className="cartItem-empty">
-            <FaCartPlus className="cartItem-empty-icon" />
-            <span>Carrinho vazio.</span>
-          </div>
-        )}
+        ))}
       </div>
-    </>
+
+      {cart.length > 0 && (
+        <div className="payment">
+          <div>
+            <h2>Resumo do pedido</h2>
+            <div className="payment-item">
+              <span>Total</span>
+              {`R$ ${price.toFixed(2)}`}
+            </div>
+            <div className="payment-item">
+              <span>Frete</span>
+              {`R$ ${frete.toFixed(2)}`}
+            </div>
+            <div className="payment-item">
+              <span>Preço total</span>
+              {`R$ ${priceTotal.toFixed(2)}`}
+            </div>
+            <div className="payment-button">
+              <button>Comprar</button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {cart.length < 1 && (
+        <div className="emptyCart">
+          <div>
+            <FaCartPlus />
+            <span>Carrinho vazio</span>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
